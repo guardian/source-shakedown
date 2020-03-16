cbae9fc21cf1d53966f2a6c110df5666ad9205d4
Simon Adcock <simonadcock2@gmail.com>
2020-03-16 14:43:18 +0000

    add checbox stories

---
+++ stories/checkbox/indeterminate.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { ThemeProvider } from "emotion-theming"
+import { storybookBackgrounds } from "@guardian/src-helpers"
+
+import {
+	Checkbox,
+	checkboxDefault,
+	checkboxBrand,
+} from "@guardian/src-checkbox"
+
+const indeterminateLight = () => (
+	<ThemeProvider theme={checkboxDefault}>
+		<Checkbox
+			indeterminate={true}
+			value="indeterminate"
+			label="Indeterminate"
+		/>
+	</ThemeProvider>
+)
+
+indeterminateLight.story = {
+	name: "indeterminate light",
+}
+
+const indeterminateBlue = () => (
+	<ThemeProvider theme={checkboxBrand}>
+		<Checkbox
+			indeterminate={true}
+			value="indeterminate"
+			label="Indeterminate"
+		/>
+	</ThemeProvider>
+)
+
+indeterminateBlue.story = {
+	name: "indeterminate blue",
+	parameters: {
+		backgrounds: [
+			Object.assign({}, { default: true }, storybookBackgrounds.brand),
+		],
+	},
+}
+
+export { indeterminateLight, indeterminateBlue }
---
+++ stories/checkbox/index.tsx
@@ -0,0 +1,10 @@
+export default {
+	title: "Checkbox",
+}
+export * from "./default"
+export * from "./error"
+export * from "./indeterminate"
+export * from "./select-all"
+export * from "./supporting-text"
+export * from "./ungrouped"
+export * from "./unlabelled"
