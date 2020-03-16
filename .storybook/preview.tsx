93df1ce8cd8c7da7b0c304b1b3dbea64dfea4df2
Simon Adcock <simonadcock2@gmail.com>
2020-03-16 14:43:33 +0000

    dont hide toolbar or nav

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,8 +4,6 @@ import { useEffect } from "@storybook/addons"
 import { FocusStyleManager } from "@guardian/src-foundations/utils"
 import { breakpoints, Breakpoint } from "@guardian/src-foundations"
 
-const isProd = process.env.NODE_ENV === "production"
-
 type ViewportMeta = {
 	[key in Breakpoint]: {
 		name: string
@@ -62,14 +60,6 @@ const viewportEntries = Object.entries(breakpoints).map(([name, width]) => {
 const viewports = Object.fromEntries(viewportEntries)
 
 addParameters({
-	// We hide the toolbar by default to make Storybook embeds
-	// look nicer in Zeroheight. Related discussion:
-	// https://github.com/storybookjs/storybook/issues/8129
-	// https://spectrum.chat/zeroheight/feature-requests/swap-out-storybook-stories-in-situ~29d7760d-aba6-4bb3-bf4a-37c119dbc622
-	options: {
-		isToolshown: !isProd,
-		isFullscreen: isProd,
-	},
 	viewport: {
 		viewports,
 		defaultViewport: "responsive",
