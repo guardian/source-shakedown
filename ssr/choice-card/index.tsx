a37f303be3066f82117b9652cbcd641326c7639c
Simon Adcock <simonadcock2@gmail.com>
2020-05-19 10:45:34 +0100

    add multi select demo to choice cards ssr

--- ssr/choice-card/index.tsx
+++ ssr/choice-card/index.tsx
@@ -12,12 +12,34 @@ const choiceCards = [
 	/>,
 	<ChoiceCard value="blue" label="Blue" id="default-blue" />,
 ]
+const multiChoiceCards = [
+	<ChoiceCard
+		value="option-1"
+		label="Option 1"
+		checked={true}
+		id="multi-1"
+	/>,
+	<ChoiceCard
+		value="option-2"
+		label="Option 2"
+		id="multi-2"
+		checked={true}
+	/>,
+	<ChoiceCard value="option-3" label="Option 3" id="multi-3" />,
+]
 /* eslint-enable react/jsx-key */
 
 export const choiceCard = (
-	<ChoiceCardGroup name="colours">
-		{choiceCards.map((choiceCard, index) =>
-			React.cloneElement(choiceCard, { key: index }),
-		)}
-	</ChoiceCardGroup>
+	<>
+		<ChoiceCardGroup name="colours">
+			{choiceCards.map((choiceCard, index) =>
+				React.cloneElement(choiceCard, { key: index }),
+			)}
+		</ChoiceCardGroup>
+		<ChoiceCardGroup name="options" multi={true}>
+			{multiChoiceCards.map((choiceCard, index) =>
+				React.cloneElement(choiceCard, { key: index }),
+			)}
+		</ChoiceCardGroup>
+	</>
 )
