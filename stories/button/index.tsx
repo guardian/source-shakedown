6ea7cce37cc5c443d4affc4cb2bff0270d424df5
Simon Adcock <simonadcock2@gmail.com>
2020-06-23 14:50:38 +0100

    refactor button stories

---
+++ stories/button/index.tsx
@@ -0,0 +1,10 @@
+export default {
+	title: "Button",
+}
+export * from "./icon-only-buttons"
+export * from "./icon-only-link-buttons"
+export * from "./priority-buttons"
+export * from "./priority-link-buttons"
+export * from "./sizes"
+export * from "./text-and-icon-buttons"
+export * from "./text-and-icon-link-buttons"
--- stories/button/priority-buttons.tsx
+++ stories/button/priority-buttons.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import { css } from "@emotion/core"
 import { storybookBackgrounds } from "@guardian/src-helpers"
-import { SvgCheckmark, SvgClose } from "@guardian/src-svgs"
-import { size, space } from "@guardian/src-foundations"
+import { space } from "@guardian/src-foundations"
 import { background } from "@guardian/src-foundations/palette"
 import {
 	Button,
-	LinkButton,
 	buttonDefault,
 	buttonBrandAlt,
 	buttonBrand,
@@ -40,53 +38,6 @@ const priorityButtons = [
 		Subdued
 	</Button>,
 ]
-const sizeButtons = [
-	<Button>Default</Button>,
-	<Button size="small">Small</Button>,
-]
-const textIconButtons = [
-	<Button icon={<SvgCheckmark />}>Icon to the left</Button>,
-	<Button iconSide="right" icon={<SvgCheckmark />}>
-		Icon to the right
-	</Button>,
-	<LinkButton href="#" showIcon={true}>
-		Link button with an arrow icon
-	</LinkButton>,
-]
-const iconButtons = [
-	<Button icon={<SvgClose />} aria-label="Dismiss the subscribe banner" />,
-	<Button
-		icon={<SvgClose />}
-		size="small"
-		aria-label="Dismiss the subscribe banner"
-	/>,
-]
-const linkButtons = [
-	<LinkButton href="#">Primary</LinkButton>,
-	<LinkButton href="#" priority="secondary">
-		Secondary
-	</LinkButton>,
-	<LinkButton href="#" priority="tertiary">
-		Tertiary
-	</LinkButton>,
-	<LinkButton href="#" priority="subdued">
-		Subdued
-	</LinkButton>,
-]
-const iconLinkButtons = [
-	<LinkButton href="#" showIcon={true}>
-		Primary
-	</LinkButton>,
-	<LinkButton href="#" showIcon={true} priority="secondary">
-		Secondary
-	</LinkButton>,
-	<LinkButton href="#" showIcon={true} priority="tertiary">
-		Tertiary
-	</LinkButton>,
-	<LinkButton href="#" showIcon={true} priority="subdued">
-		Subdued
-	</LinkButton>,
-]
 /* eslint-enable react/jsx-key */
 
 const flexStart = css`
@@ -95,14 +46,9 @@ const flexStart = css`
 	justify-content: flex-start;
 
 	> div {
-		margin-right: ${size.medium}px;
+		margin-right: ${space[9]}px;
 	}
 `
-
-export default {
-	title: "Button",
-	component: Button,
-}
 
 export const priorityLight = () => (
 	<ThemeProvider theme={buttonDefault}>
@@ -224,60 +170,3 @@ priorityReaderRevenueYellow.story = {
 		],
 	},
 }
-
-export const sizes = () => (
-	<div css={flexStart}>
-		{sizeButtons.map((button, index) => (
-			<div key={index}>{button}</div>
-		))}
-	</div>
-)
-sizes.story = {
-	name: "sizes",
-}
-
-export const textAndIcon = () => (
-	<div css={flexStart}>
-		{textIconButtons.map((button, index) => (
-			<div key={index}>{button}</div>
-		))}
-	</div>
-)
-textAndIcon.story = {
-	name: "text and icon",
-}
-
-export const iconOnly = () => (
-	<div css={flexStart}>
-		{iconButtons.map((button, index) => (
-			<div key={index}>{button}</div>
-		))}
-	</div>
-)
-iconOnly.story = {
-	name: "icon only",
-}
-
-const spacer = css`
-	margin-bottom: ${space[3]}px;
-`
-
-export const priorityLinkButtons = () => (
-	<>
-		<div css={spacer}>
-			<div css={flexStart}>
-				{linkButtons.map((button, index) => (
-					<div key={index}>{button}</div>
-				))}
-			</div>
-		</div>
-		<div css={spacer}>
-			<div css={flexStart}>
-				{iconLinkButtons.map((button, index) => (
-					<div key={index}>{button}</div>
-				))}
-			</div>
-		</div>
-	</>
-)
-priorityLinkButtons.story = { name: "priority link buttons" }
