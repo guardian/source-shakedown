df80937dc796dca0622a2b1d1c00cbc4178c6c49
Simon Adcock <simonadcock2@gmail.com>
2020-10-15 11:16:36 +0100

    upgrade to 2.5.0-rc.0

---
+++ stories/layout/columns.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Columns } from "@guardian/src-layout"
+
+const gridStoryWrapper = (storyFn: () => JSX.Element) => {
+	// override 8px margin applied globally to every preview body
+	return <div style={{ margin: "0 -8px" }}>{storyFn()}</div>
+}
+
+export default {
+	title: "Columns",
+	component: Columns,
+	decorators: [gridStoryWrapper],
+}
+
+export * from "./columns/collapse-below"
+export * from "./columns/default"
+export * from "./columns/with-width"
---
+++ stories/layout/hide.stories.tsx
@@ -0,0 +1,8 @@
+import { Hide } from "@guardian/src-layout"
+
+export default {
+	title: "Hide",
+	component: Hide,
+}
+
+export * from "./hide/default"
