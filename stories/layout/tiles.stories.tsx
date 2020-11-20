4fac8aa5410061eb5b898070bbcac01ab8772a83
Simon Adcock <simonadcock2@gmail.com>
2020-11-20 13:55:25 +0000

    verbump to 2.7.0-rc.0

---
+++ stories/layout/tiles.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Tiles } from "@guardian/src-layout"
+
+const gridStoryWrapper = (storyFn: () => JSX.Element) => {
+	// override 8px margin applied globally to every preview body
+	return <div style={{ margin: "0 -8px" }}>{storyFn()}</div>
+}
+
+export default {
+	title: "Tiles",
+	component: Tiles,
+	decorators: [gridStoryWrapper],
+}
+
+export * from "./tiles/default"
+export * from "./tiles/collapse-below"
