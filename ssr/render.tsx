b99028db80bef0902fc2b6ee41a7ccf1e247f4ea
Simon Adcock <simonadcock2@gmail.com>
2021-01-05 11:31:20 +0000

    upgrade emotion in ssr demo

--- ssr/render.tsx
+++ ssr/render.tsx
@@ -1,12 +1,13 @@
 import React, { ReactElement } from "react"
-import { CacheProvider } from "@emotion/core"
+import { CacheProvider } from "@emotion/react"
 import { renderToString } from "react-dom/server"
-import createEmotionServer from "create-emotion-server"
+import createEmotionServer from "@emotion/server/create-instance"
 import createCache from "@emotion/cache"
 import { htmlDocument } from "./html-document"
 
 export const render = (title: string, element: ReactElement): string => {
-	const cache = createCache()
+	const key = "custom"
+	const cache = createCache({ key })
 	const { extractCritical } = createEmotionServer(cache)
 	const { html, css, ids } = extractCritical(
 		renderToString(<CacheProvider value={cache}>{element}</CacheProvider>),
